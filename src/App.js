34a20e2a9f172b1ba7394a85c5716c6a894e16a8
Dora Kondala <kondaladorababu@gmail.com>
2023-10-17 22:27:17 +0530

    Banner compo Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import Banner from './components/Banner';
 import Row from './components/Row';
 import requests from './requests';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Large Banner comes Here</h1>
-      <Row title={"NETFLIX ORIGINALS"} fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
+    <div className="app">
+      {/* <h1>Large Banner comes Here</h1> */}
+      <Banner />
+      <Row title={"NETFLIX ORIGINALS"} fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
       <Row title={"Trending Now"} fetchUrl={requests.fetchTrending} />
       <Row title={"Top Rated"} fetchUrl={requests.fetchTopRated} />
       <Row title={"Action Movies"} fetchUrl={requests.fetchActionMovies} />
